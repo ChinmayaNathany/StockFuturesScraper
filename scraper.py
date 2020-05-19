8b131fd49e2bc54e68d8a0136a9229ae22b35f13
Chinmaya Nathany <chinmaya.nathany@mail.utoronto.ca>
2020-05-20 03:32:43 +0530

    Added multiple date functionality

--- scraper.py
+++ scraper.py
@@ -1,7 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
-master_data = {}
+import json
 websites_list = []
+from datetime import date
+
+try:
+    with open("data.json", "r") as fb:
+        master_data = json.load(fb)
+except:
+    master_data = {}
+    print("NEW DATABASE")
 
 with open('websites.txt') as websites:
     for website in websites:
@@ -16,10 +24,25 @@ for website in websites_list:
     stock_name = soup.find('h1', class_='pcstname').text
     open_interest_data = open_interest_data.replace(",", '')
     open_interest_data = open_interest_data.split("\n")
+    if stock_name not in master_data:
+        master_data[stock_name] = []
 
-    master_data[stock_name] = {"Open Interest": int(open_interest_data[1]), "Open Interest Change":
-        int(open_interest_data[5]), "Change Percent": float(open_interest_data[9])}
+    price_data = soup.find('div', class_='pcnsb div_live_price_wrap').text
+    price_data = price_data.replace(",", '')
+    price_data = price_data.split("\n")
+    ltp = float(price_data[1])
+    price_change = float(price_data[3][:price_data[3].find(' ')])
+    price_change_pct = float(price_data[3][price_data[3].find(' ') + 2: len(price_data[3]) - 2])
+    master_data[stock_name].append({date.today().strftime('%d/%m'): {"LTP": ltp,
+        "Price Change": price_change, "Price Change Percent": price_change_pct,
+        "Open Interest": int(open_interest_data[1]), "Open Interest Change":
+        int(open_interest_data[5]), "OI Change Percent": float(open_interest_data[9])}})
 
-print(master_data)
+
+json_to_save = json.dumps(master_data)
+with open("data.json", "w") as fb:
+    fb.write(json_to_save)
+
+#print(date.today())
 
 
