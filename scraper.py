7138a8b516eb1d971389807009a1e7b0d0c1a87c
Chinmaya Nathany <chinmaya.nathany@mail.utoronto.ca>
2020-05-20 04:47:25 +0530

    Updated data storage format

--- scraper.py
+++ scraper.py
@@ -34,10 +34,14 @@ for website in websites_list:
     ltp = float(price_data[1])
     price_change = float(price_data[3][:price_data[3].find(' ')])
     price_change_pct = float(price_data[3][price_data[3].find(' ') + 2: len(price_data[3]) - 2])
-    master_data[stock_name][date.today().strftime('%d/%m')] = {"LTP": ltp,
-        "Price Change": price_change, "Price Change Percent": price_change_pct,
-        "Open Interest": int(open_interest_data[1]), "Open Interest Change":
-        int(open_interest_data[5]), "OI Change Percent": float(open_interest_data[9])}
+    # master_data[stock_name][date.today().strftime('%d/%m')] = {"LTP": ltp,
+    #     "Price Change": price_change, "Price Change Percent": price_change_pct,
+    #     "Open Interest": int(open_interest_data[1]), "Open Interest Change":
+    #     int(open_interest_data[5]), "OI Change Percent": float(open_interest_data[9])}
+    master_data[stock_name][date.today().strftime('%d/%m')] = [ltp, price_change, price_change_pct,
+                                                               int(open_interest_data[1]), int(open_interest_data[5]),
+                                                               float(open_interest_data[9])]
+
 
 
 json_to_save = json.dumps(master_data)
